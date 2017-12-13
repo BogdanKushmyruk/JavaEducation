a513f9f059995abed2d0d6bf8d39894da1f12c74
Bogdan Kushmyruk <b.kushmyruk@gmail.com>
2017-12-13 20:47:07 +0200

    Added Calculator

---
+++ src/main/java/com/bk/Calculator.java
@@ -0,0 +1,20 @@
+package com.bk;
+
+public class Calculator {
+
+    public static double add(double number1, double number2) {
+        return number1 + number2;
+    }
+    public static double sub(double number1, double number2) {
+        return number1 - number2;
+    }
+    public static double divine (double number1, double number2) {
+        return number1 / number2;
+    }
+    public static double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
+
+}
+
+
--- src/main/java/com/bk/Main.java
+++ src/main/java/com/bk/Main.java
@@ -1,31 +1,38 @@
 package com.bk;
 
-
 import com.bk.animals.Cat;
+
+import java.util.Scanner;
 
 public class Main {
     private String message;
 
 
     public static void main(String[] args) {
-        int number = 5;
-        float floatNumber = 0.3f;
-        double doubleNumber =0.3d;
-        char key = 's';
-        char key2 = '\uAF39';
-        boolean isValid = true;
+        Cat cat = new Cat("dsa", 4);
+        Scanner scanner = new Scanner(System.in);
+        double number1 = Double.parseDouble(scanner.next());
+        double number2 = Double.parseDouble(scanner.next());
+        String command = scanner.next();
 
-        Cat zsdasdasdasds = new Cat("Tomas", 4);
+        switch (command) {
+            case "+":
+                System.out.println(Calculator.add(number1, number2));
+                break;
+            case "-":
+                System.out.println(Calculator.sub(number1, number2));
+                break;
+            case "/":
+                System.out.println(Calculator.divine(number1, number2));
+                break;
+            case "*":
+                System.out.println(Calculator.multiply(number1, number2));
+                break;
+            default:
+                System.out.println("Error command");
+        }
 
-
-        System.out.println("Hello World");
     }
 
-    public String getMessage() {
-        return message;
-    }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
